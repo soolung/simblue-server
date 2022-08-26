7858ae31b34a1297291034565c4b327f41c10a00
gimhanul <gimhanuly@gmail.com>
2022-08-26 14:01:57 +0900

    init

---
+++ src/main/java/com/soogung/simblue/SimblueApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.simblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimblueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimblueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamdoliro/simblue/SimblueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.simblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimblueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
