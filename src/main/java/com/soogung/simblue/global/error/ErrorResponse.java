13f259309fec8ff047fe13e9ce77ca6f5c7aa356
gimhanul <gimhanuly@gmail.com>
2022-08-31 17:54:48 +0900

    FIX :: ErrorResponse CI Failed
    - builder와 전체 생성자가 있어야댐

--- src/main/java/com/soogung/simblue/global/error/ErrorResponse.java
+++ src/main/java/com/soogung/simblue/global/error/ErrorResponse.java
@@ -1,11 +1,13 @@
 package com.soogung.simblue.global.error;
 
 import com.soogung.simblue.global.error.exception.ErrorCode;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class ErrorResponse {
 
     private int status;
