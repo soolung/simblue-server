8411f8369653f21fcdda88f0515c113a6476c236
gimhanul <gimhanuly@gmail.com>
2022-09-01 23:38:50 +0900

    FIX :: rest controller not found
    - package 설정 개버그...

--- src/main/java/com/soogung/simblue/SimblueApplication.java
+++ src/main/java/com/soogung/simblue/SimblueApplication.java
@@ -1,4 +1,4 @@
-package com.bamdoliro.simblue;
+package com.soogung.simblue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
