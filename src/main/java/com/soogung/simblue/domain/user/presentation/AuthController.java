0d633b9e054b08e84b48b01932251084f9c0633d
gimhanul <gimhanuly@gmail.com>
2022-09-01 23:43:21 +0900

    ADD :: get google auth link logic (#3)
    - configuration properties 신기하ㅔㄴ요
    - 테스트두함

--- src/main/java/com/soogung/simblue/domain/user/presentation/AuthController.java
+++ src/main/java/com/soogung/simblue/domain/user/presentation/AuthController.java
@@ -1,4 +1,31 @@
 package com.soogung.simblue.domain.user.presentation;
 
+import com.soogung.simblue.domain.user.presentation.dto.request.AuthRequest;
+import com.soogung.simblue.domain.user.service.GetGoogleAuthLinkService;
+import com.soogung.simblue.domain.user.service.JoinWithGoogleService;
+import com.soogung.simblue.global.feign.auth.dto.response.TokenResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Slf4j
 public class AuthController {
+
+    private final GetGoogleAuthLinkService getGoogleAuthLinkService;
+    private final JoinWithGoogleService joinWithGoogleService;
+
+    @GetMapping("/google")
+    public String getGoogleAuthLink() {
+        return getGoogleAuthLinkService.execute();
+    }
+
+    @PostMapping("/google")
+    public TokenResponse joinWithGoogle(@RequestBody @Valid AuthRequest request) {
+        return joinWithGoogleService.execute(request);
+    }
 }
--- src/main/java/com/soogung/simblue/domain/user/service/AuthService.java
+++
@@ -1,4 +0,0 @@
-package com.soogung.simblue.domain.user.service;
-
-public class AuthService {
-}
