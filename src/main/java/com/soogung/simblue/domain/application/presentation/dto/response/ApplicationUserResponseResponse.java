b005a5790a7afcfe9cbd41d16ae30821122b2a78
gimhanul <gimhanuly@gmail.com>
2022-12-01 15:09:54 +0900

    CHORE :: response dto data 형식 변경

--- src/main/java/com/soogung/simblue/domain/application/presentation/dto/response/ApplicationUserResponseResponse.java
+++ src/main/java/com/soogung/simblue/domain/application/presentation/dto/response/ApplicationUserResponseResponse.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @AllArgsConstructor
 public class ApplicationUserResponseResponse {
     private String name;
     private String studentNumber;
-    private Map<Long, List<ApplicationRequestResponse>> answerList;
+    private List<String> answerList;
 }
--- src/main/java/com/soogung/simblue/domain/application/service/QueryApplicationResultService.java
+++ src/main/java/com/soogung/simblue/domain/application/service/QueryApplicationResultService.java
@@ -1,12 +1,11 @@
 package com.soogung.simblue.domain.application.service;
 
 import com.soogung.simblue.domain.application.domain.ApplicationQuestion;
+import com.soogung.simblue.domain.application.domain.ApplicationRequest;
 import com.soogung.simblue.domain.application.domain.ApplicationRequestBlock;
 import com.soogung.simblue.domain.application.domain.repository.ApplicationOwnerRepository;
 import com.soogung.simblue.domain.application.domain.repository.ApplicationQuestionRepository;
 import com.soogung.simblue.domain.application.domain.repository.ApplicationRequestBlockRepository;
-import com.soogung.simblue.domain.application.facade.ApplicationFacade;
-import com.soogung.simblue.domain.application.presentation.dto.response.ApplicationRequestResponse;
 import com.soogung.simblue.domain.application.presentation.dto.response.ApplicationResultResponse;
 import com.soogung.simblue.domain.application.presentation.dto.response.ApplicationUserResponseResponse;
 import com.soogung.simblue.domain.user.domain.Student;
@@ -14,20 +13,20 @@ import com.soogung.simblue.domain.user.domain.Teacher;
 import com.soogung.simblue.domain.user.exception.AuthorityMismatchException;
 import com.soogung.simblue.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class QueryApplicationResultService {
 
     private final UserFacade userFacade;
     private final ApplicationOwnerRepository applicationOwnerRepository;
-    private final ApplicationFacade applicationFacade;
     private final ApplicationQuestionRepository applicationQuestionRepository;
     private final ApplicationRequestBlockRepository applicationRequestBlockRepository;
 
@@ -61,15 +60,22 @@ public class QueryApplicationResultService {
 
     private ApplicationUserResponseResponse createApplicationUserResponseResponse(ApplicationRequestBlock block) {
         Student student = block.getStudent();
-        Map<Long, List<ApplicationRequestResponse>> answerList = block.getRequests().stream()
-                .map(ApplicationRequestResponse::of)
-                .collect(Collectors.groupingBy(ApplicationRequestResponse::getQuestionId));
-
+        List<String> answerList = block.getRequests().stream()
+                .collect(Collectors.groupingBy(r -> r.getApplicationQuestion().getId()))
+                .values().stream()
+                .map(this::getResult)
+                .collect(Collectors.toList());
 
         return new ApplicationUserResponseResponse(
                 student.getUser().getName(),
                 student.getStudentNumber(),
                 answerList
         );
+    }
+
+    private String getResult(List<ApplicationRequest> request) {
+        return request.stream()
+                .map(ApplicationRequest::getAnswer)
+                .collect(Collectors.joining(", "));
     }
 }
