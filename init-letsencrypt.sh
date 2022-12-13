a67c8142e387c8dd45315738f3b9250bcaebb9eb
gimhanul <gimhanuly@gmail.com>
2022-12-13 20:18:55 +0900

    CHORE :: ssl...

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="api.soolung.kro.kr"
+domains="api.soolung.ga"
 rsa_key_size=4096
 data_path="./data/certbot"
 email="dev.gimhanul@gmail.com" # Adding a valid address is strongly recommended
