f37c51187a55d2d83d164f3b8325b900faca1b74
gimhanul <gimhanuly@gmail.com>
2023-02-05 21:47:25 +0900

    ADD :: 선생님 검색 API (#50)

--- src/main/java/com/soogung/simblue/domain/user/domain/repository/TeacherRepository.java
+++ src/main/java/com/soogung/simblue/domain/user/domain/repository/TeacherRepository.java
@@ -3,10 +3,16 @@ package com.soogung.simblue.domain.user.domain.repository;
 import com.soogung.simblue.domain.user.domain.Teacher;
 import com.soogung.simblue.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
     Optional<Teacher> findByUser(User user);
+
+    @Query("SELECT t FROM Teacher t JOIN FETCH t.user WHERE t.user.authority = 'ROLE_TEACHER' AND t.user.name LIKE %:name%")
+    List<Teacher> searchTeacherByName(@Param("name") String name);
 }
--- src/main/java/com/soogung/simblue/domain/user/presentation/TeacherController.java
+++ src/main/java/com/soogung/simblue/domain/user/presentation/TeacherController.java
@@ -2,27 +2,36 @@ package com.soogung.simblue.domain.user.presentation;
 
 import com.soogung.simblue.domain.user.presentation.dto.request.TeacherRequest;
 import com.soogung.simblue.domain.user.presentation.dto.request.UpdateTeacherRequest;
+import com.soogung.simblue.domain.user.presentation.dto.response.TeacherResponse;
 import com.soogung.simblue.domain.user.service.JoinTeacherService;
+import com.soogung.simblue.domain.user.service.SearchTeacherService;
 import com.soogung.simblue.domain.user.service.UpdateTeacherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/teacher")
 @RequiredArgsConstructor
 public class TeacherController {
 
+    private final SearchTeacherService searchTeacherService;
     private final JoinTeacherService joinTeacherService;
     private final UpdateTeacherService updateTeacherService;
 
-    @PostMapping("/teacher")
+    @GetMapping("/search")
+    public List<TeacherResponse> searchTeacher(@RequestParam(name = "q") String q) {
+        return searchTeacherService.execute(q);
+    }
+
+    @PostMapping
     public void joinTeacher(@RequestBody @Valid TeacherRequest request) {
         joinTeacherService.execute(request);
     }
 
-    @PutMapping("/teacher")
+    @PutMapping
     public void updateTeacher(@RequestBody @Valid UpdateTeacherRequest request) {
         updateTeacherService.execute(request);
     }
