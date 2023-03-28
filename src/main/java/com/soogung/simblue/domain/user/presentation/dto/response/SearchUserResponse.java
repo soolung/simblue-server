c7465575ca71870c80c713a8f4dfa34dbdd66811
gimhanul <gimhanuly@gmail.com>
2023-03-28 21:16:30 +0900

    ADD :: 검색 결과에 학번 추가 (#64)

--- src/main/java/com/soogung/simblue/domain/user/presentation/dto/response/SearchUserResponse.java
+++ src/main/java/com/soogung/simblue/domain/user/presentation/dto/response/SearchUserResponse.java
@@ -2,6 +2,7 @@ package com.soogung.simblue.domain.user.presentation.dto.response;
 
 import com.soogung.simblue.domain.user.domain.User;
 import com.soogung.simblue.domain.user.domain.type.Authority;
+import com.soogung.simblue.domain.user.facade.UserFacade;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,12 +12,15 @@ public class SearchUserResponse {
 
     private Long userId;
     private String name;
+    private String studentNumber;
     private Authority authority;
 
-    public static SearchUserResponse of(User user) {
+    public static SearchUserResponse of(User user, UserFacade userFacade) {
         return SearchUserResponse.builder()
                 .userId(user.getId())
                 .name(user.getName())
+                .studentNumber(user.getAuthority() == Authority.ROLE_STUDENT ?
+                        userFacade.findStudentByUser(user).getStudentNumber() : null)
                 .authority(user.getAuthority())
                 .build();
     }
--- src/main/java/com/soogung/simblue/domain/user/service/SearchUserService.java
+++ src/main/java/com/soogung/simblue/domain/user/service/SearchUserService.java
@@ -2,6 +2,7 @@ package com.soogung.simblue.domain.user.service;
 
 import com.soogung.simblue.domain.user.domain.repository.UserRepository;
 import com.soogung.simblue.domain.user.domain.type.Authority;
+import com.soogung.simblue.domain.user.facade.UserFacade;
 import com.soogung.simblue.domain.user.presentation.dto.response.SearchUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,11 @@ import java.util.stream.Collectors;
 public class SearchUserService {
 
     private final UserRepository userRepository;
+    private final UserFacade userFacade;
 
     public List<SearchUserResponse> execute(String q, Authority authority) {
         return userRepository.searchUser(q, authority)
-                .stream().map(SearchUserResponse::of)
+                .stream().map(u -> SearchUserResponse.of(u, userFacade))
                 .collect(Collectors.toList());
     }
 }
