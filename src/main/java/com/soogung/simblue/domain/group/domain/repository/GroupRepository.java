f8d7f2e1eeac4ae273504bbd44bd30591b555ce2
YuLim2 <jyulim0120@gmail.com>
2023-03-30 17:35:19 +0900

    FIX: worng spelling

--- src/main/java/com/soogung/simblue/domain/group/domain/repository/GroupRepository.java
+++ src/main/java/com/soogung/simblue/domain/group/domain/repository/GroupRepository.java
@@ -3,7 +3,8 @@ package com.soogung.simblue.domain.group.domain.repository;
 import com.soogung.simblue.domain.group.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 
-lsimport java.util.List;
+import java.util.List;
+
 
 public interface GroupRepository extends CrudRepository<Group, Long> {
 
