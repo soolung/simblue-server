e0a8b65bfcb86c88b078cd94c9e35d887e82dbd6
YuLim2 <jyulim0120@gmail.com>
2023-03-30 15:52:38 +0900

    ADD: Create group

---
+++ src/main/java/com/soogung/simblue/domain/group/presentation/GroupController.java
@@ -0,0 +1,32 @@
+package com.soogung.simblue.domain.group.presentation;
+
+import com.soogung.simblue.domain.group.presentation.dto.request.GroupRequest;
+import com.soogung.simblue.domain.group.presentation.dto.response.GroupListResponse;
+import com.soogung.simblue.domain.group.service.CreateGroupService;
+import com.soogung.simblue.domain.group.service.QueryGroupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/group")
+@RequiredArgsConstructor
+public class GroupController {
+
+    private final CreateGroupService createGroupService;
+
+    private final QueryGroupService queryGroupService;
+
+    @PostMapping
+    public void createGroup(@RequestBody @Valid GroupRequest request) {
+        createGroupService.execute(request);
+    }
+
+    @GetMapping
+    public GroupListResponse getListGroups(){
+        return queryGroupService.execute();
+    }
+
+}
