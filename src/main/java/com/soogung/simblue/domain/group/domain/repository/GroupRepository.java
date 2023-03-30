231f849beb85de6ad6942d8a8d64b39144f3bf12
YuLim2 <jyulim0120@gmail.com>
2023-03-30 15:51:28 +0900

    FEAT: findAll function overloading

--- src/main/java/com/soogung/simblue/domain/group/domain/repository/GroupRepository.java
+++ src/main/java/com/soogung/simblue/domain/group/domain/repository/GroupRepository.java
@@ -3,5 +3,9 @@ package com.soogung.simblue.domain.group.domain.repository;
 import com.soogung.simblue.domain.group.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 
+lsimport java.util.List;
+
 public interface GroupRepository extends CrudRepository<Group, Long> {
+
+    List<Group> findAll();
 }
